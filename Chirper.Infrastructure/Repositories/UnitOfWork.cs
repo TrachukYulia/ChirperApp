d8175bd6a966a75e1ce5bdd27033fa2a3d55d6f5
GreenSky <trachukyulia77@gmail.com>
2023-10-09 00:59:16 +0300

    added unit of work

---
+++ Chirper.Infrastructure/Repositories/UnitOfWork.cs
@@ -0,0 +1,24 @@
+﻿using Chirper.Application.Repositories;
+using Chirper.Infrastructure.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Chirper.Application.Repositories;
+namespace Chirper.Infrastructure.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly DataContext _context;
+
+        public UnitOfWork(DataContext context)
+        {
+            _context = context;
+        }
+        public Task Save()
+        {
+            return _context.SaveChangesAsync();
+        }
+    }
+}
