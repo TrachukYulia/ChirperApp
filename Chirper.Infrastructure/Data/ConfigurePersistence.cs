d540737881f780153f53f8fbdb444f75d4d15be9
GreenSky <trachukyulia77@gmail.com>
2023-10-14 00:42:52 +0300

    added dbcontext and registration extensions

---
+++ Chirper.Infrastructure/Data/ConfigurePersistence.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using Chirper.Application.Repositories;
+using Chirper.Infrastructure.Repositories;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chirper.Infrastructure.Data
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigurePersistence(this IServiceCollection services, IConfiguration configuration)
+        {
+
+            services.AddDbContext<ChirperContext>(options =>
+               options.UseNpgsql(
+               configuration.GetConnectionString("DefaultConnection"))
+               );
+        }
+    }
+}
---
+++ Chirper.Infrastructure/Data/DbContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Chirper.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Chirper.Infrastructure.Data
+{
+    public class ChirperContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public ChirperContext(DbContextOptions<ChirperContext> options) : base(options)
+        {
+        }
+        public ChirperContext() : base() { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
